5d74a5533f0d5ac6d7e1a1b092be2c8bd2b4505f
plecart <pacome@lecart.dev>
2023-07-29 16:44:28 +0200

    Feat - Ajout de del et put, server prés

--- src/game_card_model.rs
+++ src/game_card_model.rs
@@ -1,14 +1,14 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Serialize, Deserialize)]
-pub enum Role {
-    Controleur,
-    Duelliste,
-    Sentinelle,
-    Initiateur
+#[derive(Clone, Copy, Serialize, Deserialize)]
+pub enum Outcome {
+    Victoire,
+    Défaite,
+    Egalité,
+    Annulé
 }
 
-#[derive(Clone, Serialize, Deserialize)]
+#[derive(Clone, Copy, Serialize, Deserialize)]
 pub enum GameType {
     NonClassé,
     Compétition,
@@ -20,20 +20,67 @@ pub enum GameType {
     PartiePersonnnalisé
 }
 
-// Fonction pour généré automatiquement un id a chaque partie
+#[derive(Clone, Copy, Serialize, Deserialize)]
+pub enum Character {
+    Astra,
+    Breach,
+    Brimstone,
+    Chamber,
+    Cypher,
+    Deadlock,
+    Fade,
+    Gekko,
+    Harbor,
+    Jett,
+    KayO,
+    Killjoy,
+    Neon,
+    Omen,
+    Pheonix,
+    Raze,
+    Reyna,
+    Sage,
+    Syke,
+    Sova,
+    Viper,
+    Yoru
+}
+
+#[derive(Clone, Copy, Serialize, Deserialize)]
+pub enum Role {
+    Controleur,
+    Duelliste,
+    Sentinelle,
+    Initiateur
+}
+
+// Fonction getter/setter
 impl GameCard {
-    pub fn set_id(&mut self, new_id: Option<i32>) {
-        self.id = new_id;
-    }
+    //pub fn get_id(&mut self) { return self.id; }
+    pub fn get_id(&self) -> Option<i32> { return self.id; }
+    pub fn get_outcome(&self) -> Outcome { return self.outcome; }
+    pub fn get_game_type(&self) -> GameType { return self.game_type; }
+    pub fn get_character(&self) -> Character { return self.character; }
+    pub fn get_kda(&self) -> [i32; 3] { return self.kda; }
+    pub fn get_role(&self) -> Role { return self.role; }
+    pub fn get_comment(&self) -> &String { return &self.comment; }
+
+    pub fn set_id(&mut self, new_id: Option<i32>) { self.id = new_id; }
+    pub fn set_outcome(&mut self, outcome: Outcome) { self.outcome = outcome; }
+    pub fn set_game_type(&mut self, game_type: GameType) { self.game_type = game_type; }
+    pub fn set_character(&mut self, character: Character) { self.character = character; }
+    pub fn set_kda(&mut self, kda: [i32; 3]) { self.kda = kda; }
+    pub fn set_role(&mut self, role: Role) { self.role = role; }
+    pub fn set_comment(&mut self, comment: &String) { self.comment = comment.clone(); }
 }
 
 // Structure de la carte répertoriant le les données de ma partie
 #[derive(Clone, Serialize, Deserialize)]
 pub struct GameCard {
     id: Option<i32>,
-    is_victory: bool,
-    game_type: GameType, // Un Enum serait aurait été plus juste ici
-    character_name: String,
+    outcome: Outcome,
+    game_type: GameType,
+    character: Character,
     kda: [i32; 3],
     role: Role,
     comment: String,
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use serde::{Deserialize, Serialize};
 use std::sync::{Arc, Mutex};
 mod game_card_model;
 use crate::game_card_model::GameCard;
-use crate::game_card_model::Role;
 
 // Initalisation du state
 #[derive(Clone, Serialize, Deserialize)]
@@ -20,14 +19,57 @@ impl AppState {
     }
 }
 
+// Route pour récupérer toute les cartes
+#[get("/gameCards")]
+async fn get_all_game_cards(data: web::Data<Arc<Mutex<AppState>>>) -> HttpResponse {
+    let state = data.lock().unwrap();
+    HttpResponse::Ok().json(&state.game_cards)
+}
+
 // Route pour créer un carte
-#[post("/gameCard")]
+#[post("/gameCards")]
 async fn create_game_card(data: web::Data<Arc<Mutex<AppState>>>, new_game_card: web::Json<GameCard>) -> impl Responder {
     let mut state = data.lock().unwrap();
     let mut local_new_game_card = new_game_card.into_inner();
     local_new_game_card.set_id(Some(state.generate_unique_id().try_into().unwrap()));
     state.game_cards.push(local_new_game_card);
     HttpResponse::Created().json(state.game_cards.last().unwrap())
+}
+
+// Route pour modifier une carte
+#[put("/gameCards/{game_card_id}")]
+async fn update_card(data: web::Data<Arc<Mutex<AppState>>>, path: web::Path<i32>, updated_game_card: web::Json<GameCard>) -> HttpResponse {
+    let game_card_id = path.0;
+    let mut state = data.lock().unwrap();
+   // print(game_card.get_id());
+    if let Some(index) = state.game_cards.iter().position(|game_card| game_card.get_id() == Some(game_card_id)) {
+        if let Some(game_card) = state.game_cards.get_mut(index) {
+            let updated_data = updated_game_card.into_inner();
+            game_card.set_outcome(updated_data.get_outcome());
+            game_card.set_game_type(updated_data.get_game_type());
+            game_card.set_character(updated_data.get_character());
+            game_card.set_kda(updated_data.get_kda());
+            game_card.set_role(updated_data.get_role());
+            game_card.set_comment(updated_data.get_comment());
+            HttpResponse::Ok().json(game_card)
+        } else {
+            HttpResponse::InternalServerError().body(format!("Failed to update the card"))
+        }
+    } else {
+        HttpResponse::NotFound().body(format!("Carte non trouvée"))
+    }
+}
+
+#[delete("/gameCards/{game_card_id}")]
+async fn delete_card(data: web::Data<Arc<Mutex<AppState>>>, path: web::Path<i32>) -> HttpResponse {
+    let game_card_id = path.0;
+    let mut state = data.lock().unwrap();
+    if let Some(index) = state.game_cards.iter().position(|card| card.get_id() == Some(game_card_id)) {
+        state.game_cards.remove(index);
+        HttpResponse::Ok().body(format!("Carte supprimée"))
+    } else {
+        HttpResponse::NotFound().body(format!("Carte non trouvée"))
+    }
 }
 
 #[actix_web::main]
@@ -42,7 +84,10 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(app_state.clone()))
+            .service(get_all_game_cards)
             .service(create_game_card)
+            .service(update_card)
+            .service(delete_card)
     })
     .bind("127.0.0.1:4242")?
     .run()
