d86e99e17da88543dec51e89f022633d09670866
plecart <pacome@lecart.dev>
2023-07-29 14:15:07 +0200

    edit - Ajout de l'enum pour game_type

--- src/game_card_model.rs
+++ src/game_card_model.rs
@@ -1,6 +1,5 @@
 use serde::{Deserialize, Serialize};
 
-// Structure de la carte répertoriant le les données de ma partie
 #[derive(Clone, Serialize, Deserialize)]
 pub enum Role {
     Controleur,
@@ -9,17 +8,31 @@ pub enum Role {
     Initiateur
 }
 
+#[derive(Clone, Serialize, Deserialize)]
+pub enum GameType {
+    NonClassé,
+    Compétition,
+    Vélocité,
+    SpikeRush,
+    CombatAMort,
+    Intensification,
+    CombatAMortPartEquipe,
+    PartiePersonnnalisé
+}
+
+// Fonction pour généré automatiquement un id a chaque partie
 impl GameCard {
     pub fn set_id(&mut self, new_id: Option<i32>) {
         self.id = new_id;
     }
 }
 
+// Structure de la carte répertoriant le les données de ma partie
 #[derive(Clone, Serialize, Deserialize)]
 pub struct GameCard {
     id: Option<i32>,
     is_victory: bool,
-    is_competitive: bool, // Un Enum serait aurait été plus juste ici
+    game_type: GameType, // Un Enum serait aurait été plus juste ici
     character_name: String,
     kda: [i32; 3],
     role: Role,
